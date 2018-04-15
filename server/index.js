7673f51ef779ebfd73cf7540b118d8f7dccfedb9
Robin Cussol <me@robincussol.com>
2018-04-15 18:46:40 +0200

    Skeleton for express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.use("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port);
+
+console.log(`Server started on port ${port}`);
