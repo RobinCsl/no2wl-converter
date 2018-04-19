ca2d085dc64c371c1ae73745150961640fc0aaa4
Robin Cussol <me@robincussol.com>
2018-04-19 14:17:50 +0200

    word-filtering branch; possible to filter results from server with respect to a predefined dictionary of English words

--- app/src/actions/wordList.js
+++ app/src/actions/wordList.js
@@ -1,12 +1,28 @@
+import Trie from "trie-js";
+import words from "../words";
+
 const requestWordList = () => ({
   type: "REQUEST_WORD_LIST",
 });
 
-const receiveWordList = payload => ({
-  type: "RECEIVE_WORD_LIST",
-  error: payload.error,
-  wordList: payload.result,
-});
+const receiveWordList = (payload) => {
+  const trie = new Trie(words);
+
+  const wordList = [];
+  if (payload && payload.result) {
+    payload.result.forEach((res) => {
+      if (trie.isPrefix(res) || trie.lookup(res)) {
+        wordList.push(res);
+      }
+    });
+  }
+
+  return {
+    type: "RECEIVE_WORD_LIST",
+    error: payload.error,
+    wordList,
+  };
+};
 
 const fetchWordList = state => (dispatch) => {
   dispatch(requestWordList());
